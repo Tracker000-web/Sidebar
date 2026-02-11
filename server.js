25aee3ada885183e9167b92b666fbeae3d60ebd7
Tracker000-web <tracker.beastmodelegacy@gmail.com>
2026-02-11 18:11:46 -0500

    added node.js to user.html

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ const io = new Server(server, { cors: { origin: '*' } });
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static(__dirname));
+
 
 // -------------------
 // MySQL connection
